bc121715e464db276ca5f7098d88f35cf1bcadfe
James <james@coastwide.systems>
2026-01-06 13:43:14 -0800

    Alter format of the expected VERSION arg to remove the "v" prefix

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 # Fail on errors
 set -e
 
-if [[ "$1" =~ ^v[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
+if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
 	VERSION="$1"
 else
 	echo "Invalid version argument: '$1'" >&2
@@ -14,8 +14,8 @@ echo "Downloading Aseprite $VERSION and compiling"
 
 git clone --recursive https://github.com/aseprite/aseprite.git
 cd aseprite
-git checkout tags/$VERSION || { echo "Failed to checkout 'tags/$VERSION'" >&2; exit 1; }
+git checkout tags/v$VERSION || { echo "Failed to checkout 'tags/v$VERSION'" >&2; exit 1; }
 ./build.sh --auto --norun || { echo "Aseprite build script failed" >&2; exit 1; }
 echo "Build complete, copying to /output"
-cp -R ./build/bin /output/aseprite-${VERSION/v/} || { echo "Failed to copy built binaries:" >&2; ls -l ./build; exit 1; }
+cp -R ./build/bin /output/aseprite-${VERSION} || { echo "Failed to copy built binaries:" >&2; ls -l ./build; exit 1; }
 echo "Done."
